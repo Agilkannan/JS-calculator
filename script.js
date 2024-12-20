667292bb647b8ecf82de5c4da1685011b66eb90d
Agilkannan <agilkannan49@gmail.com>
2024-12-20 23:34:12 +0530

    Basic calculator

---
+++ script.js
@@ -0,0 +1,14 @@
+function displayValue(val){
+    document.getElementById("display").value = document.getElementById("display").value+val;
+}
+
+function displayClear(){
+    document.getElementById("display").value=""
+}
+
+function Calculate(){
+    var UserInput = document.getElementById("display").value;
+    var Result = eval(UserInput);
+    document.getElementById("display").value=Result;
+
+}
