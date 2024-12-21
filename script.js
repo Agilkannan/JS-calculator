cb2192ae8342873e4bbe558ab0ef0836b057b5dd
Agilkannan <agilkannan49@gmail.com>
2024-12-21 16:43:09 +0530

    Updated calculator

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function displayValue(val){
-    document.getElementById("display").value = document.getElementById("display").value+val;
+function displayValue(val) {
+    document.getElementById("display").value = document.getElementById("display").value + val;
 }
 
-function displayClear(){
-    document.getElementById("display").value=""
+function displayClear() {
+    document.getElementById("display").value = ""
 }
 
-function Calculate(){
+function Calculate() {
     var UserInput = document.getElementById("display").value;
     var Result = eval(UserInput);
-    document.getElementById("display").value=Result;
+    document.getElementById("display").value = Result;
 
 }
